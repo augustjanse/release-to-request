40c2dfe7c88b0e4585e4179fcd5a88ab9a984b06
augustjanse <augustj@kth.se>
2016-01-12 14:46:55 +0100

    Make script add URLs from MusicBrainz to description
    Fix #2.

--- Release_to_request.user.js
+++ Release_to_request.user.js
@@ -20,13 +20,25 @@ if(debugging) {
 function importMetadata(){
 	var input = $("#mbid_box").val();
 	var mbid = input.match("[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+")[0];
-	var url = 'http://musicbrainz.org/release-group/' + mbid;
+	url = 'http://musicbrainz.org/release-group/' + mbid;
+
+	// get release group metadata
 	$.ajax({
 		method: 'GET',
 		url: 'https://musicbrainz.org/ws/2/release-group/' + mbid + '?inc=artists',
-	       	success: enterMetadata,
+	       	success: enterReleaseGroup,
 		error: function(xml) {
-			console.log("MB request failed");
+			console.log("Release group request failed");
+		}
+	});
+
+	// get release metadata
+	$.ajax({
+		method: 'GET',
+		url: 'https://musicbrainz.org/ws/2/release?release-group=' + mbid + '&inc=url-rels',
+	       	success: enterRelease,
+		error: function(xml) {
+			console.log("Release request failed");
 		}
 	});
 
@@ -34,13 +46,9 @@ function importMetadata(){
 	$('#toggle_formats').click();
 	$('#toggle_bitrates').click();
 	$('#toggle_media').click();
-
-	var text = "Any release of this accepted.\n\n";
-	text += url;
-	$('textarea[name="description"]').text(text);
 }
 
-function enterMetadata(xml) {
+function enterReleaseGroup(xml) {
 	// get metadata from XML
 	var artist = $(xml).find("name").text();	
 	var title = $(xml).find("title").text();
@@ -53,4 +61,38 @@ function enterMetadata(xml) {
 	$('[name="year"]').val(year);	
 	var dd_val = $('select option').filter(function () { return $(this).html() == type;}).val(); 
 	$('#releasetype').val(dd_val);
+}
+
+function enterRelease(xml) {
+	// get URL relationships
+	var downloadFree = ""; // "download for free"
+	var downloadPurchase = ""; // "purchase for download"
+	var mailOrderPurchase = ""; // "purchase for mail-order"
+	var amazon = ""; // "amazon asin"
+
+	text = "Any release of this accepted.\n\n";
+	text += "MusicBrainz\n" + url + "\n\n";
+
+	listUrls(xml, "download for free", "Download for free");
+	listUrls(xml, "purchase for download", "Purchase for download");
+	listUrls(xml, "purchase for mail-order", "Purchase for mail-order");
+	listUrls(xml, "amazon asin", "Amazon");
+
+	$('textarea[name="description"]').text(text.trim());
+}
+
+function listUrls(xml, type, intro) {
+	var list = "";
+
+	$(xml).find('[type="' + type + '"] target').each(function() {
+		if (list == "") {
+			list += intro + "\n";
+		}
+
+		list += $(this).text() + "\n";
+	});
+	
+	if (list != "") {	
+		text += list + "\n";
+	}
 }
